35ee0e528237a05dfd772f16debdb1412687b04b
KIMYUNMEE <jayoo8665@naver.com>
2022-06-30 02:44:16 +0900

    :lipstick: Update Stastic component 디자인
    styled-components 이용하여 화면 구성 및 export한 fontStyle 상수 import

--- src/components/Stastic.tsx
+++ src/components/Stastic.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import {
+  STASTIC_CONTENT_COLOR,
+  STASTIC_CONTENT_FONTSTYLE,
+} from 'styles/fontStyle'
+
+const StasticContainer = styled.div`
+  margin-left: 623px;
+  padding-top: 150px;
+`
+const StasticItem = styled.div`
+  margin-bottom: 20px;
+  font-size: ${STASTIC_CONTENT_FONTSTYLE}px;
+  font-weight: bold;
+  font-family: sans-serif;
+  letter-spacing: -1px;
+  color: ${STASTIC_CONTENT_COLOR};
+`
 
 const Stastic: React.FC = () => {
   return (
     <div>
-      <strong>만 명</strong>의 사용자
-      <strong>만 개</strong>의 리뷰
-      <strong>만 개</strong>의 저장
+      <StasticContainer>
+        <StasticItem>
+          <strong>만 명</strong>의 사용자
+        </StasticItem>
+        <StasticItem>
+          <strong>만 개</strong>의 리뷰
+        </StasticItem>
+        <StasticItem>
+          <strong>만 개</strong>의 저장
+        </StasticItem>
+      </StasticContainer>
     </div>
   )
 }
