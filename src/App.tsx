f36537d302e86e0eab9918662955d344d3720a5c
KIMYUNMEE <jayoo8665@naver.com>
2022-06-30 01:19:51 +0900

    :memo: Add Awards component 생성
    Awards component 생성 및 App.tsx연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import TripleLogo from 'components/TripleLogo'
+import Awards from 'components/Awards'
 
 const ContentContainer = styled.div`
   position: relative;
@@ -10,12 +11,14 @@ const ContentContainer = styled.div`
   width: 1200px;
   height: auto;
 `
+
 const App: React.FC = () => {
   return (
     <div>
       <main>
         <ContentContainer>
           <TripleLogo />
+          <Awards />
         </ContentContainer>
       </main>
     </div>
---
+++ src/components/Awards.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Awards: React.FC = () => {
+  return (
+    <div>
+      2018 구글 플레이스토어
+      <br />
+      올해의 앱 최우수상 수상 2018 애플 앱스토어
+      <br />
+      오늘의 여행앱 선정
+    </div>
+  )
+}
+
+export default Awards
