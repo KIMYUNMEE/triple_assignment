7bf2aaf339114d1bc5ef54a677b329f70ea92a97
KIMYUNMEE <jayoo8665@naver.com>
2022-06-29 16:28:13 +0900

    :lipstick: Update export한 상수 import하기
    styled-components를 이용해 디자인하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import TripleLogo from 'components/TripleLogo'
 
+const ContentContainer = styled.div`
+  position: relative;
+  margin: 0 auto;
+  padding: 50px 0;
+  width: 1200px;
+  height: auto;
+`
 const App: React.FC = () => {
   return (
     <div>
       <main>
-        <TripleLogo />
+        <ContentContainer>
+          <TripleLogo />
+        </ContentContainer>
       </main>
     </div>
   )
--- src/components/TripleLogo.tsx
+++ src/components/TripleLogo.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import { TRIPLE_LOGO_IMG } from 'styles/image'
+import {
+  TRIPLE_LOGO_CONTENT_COLOR,
+  TRIPLE_LOGO_CONTENT_FONTSTYLE,
+} from 'styles/fontStyle'
+
+const LogoContainer = styled.div`
+  padding-top: 280px;
+  width: 400px;
+  height: 338px;
+  box-sizing: border-box;
+  background-image: url(${TRIPLE_LOGO_IMG});
+  background-repeat: no-repeat;
+  background-size: 400px 338px;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: ${TRIPLE_LOGO_CONTENT_FONTSTYLE}px;
+  font-weight: bold;
+  color: ${TRIPLE_LOGO_CONTENT_COLOR};
+`
 const TripleLogo: React.FC = () => {
-  return <div>2019년 2월 기준</div>
+  return <LogoContainer>2019년 2월 기준</LogoContainer>
 }
 
 export default TripleLogo
