0c1a4c9643bad49383ab339253329c0bdda29d72
KIMYUNMEE <jayoo8665@naver.com>
2022-06-29 16:22:00 +0900

    :lipstick: Add image.ts , fontStyle.ts import 후 export하기
    images파일과 color, font-size 상수에 담기

---
+++ src/styles/fontStyle.ts
@@ -0,0 +1,2 @@
+export const TRIPLE_LOGO_CONTENT_COLOR = 'rgba(58, 58, 58, 0.7)'
+export const TRIPLE_LOGO_CONTENT_FONTSTYLE = '15'
---
+++ src/styles/image.ts
@@ -0,0 +1,3 @@
+import TripleIMG from 'images/triple2x.png'
+
+export const TRIPLE_LOGO_IMG = TripleIMG
