942e74ce2c94f661f664276fa1eb705d892db136
KIMYUNMEE <jayoo8665@naver.com>
2022-06-30 18:03:49 +0900

    :poop: Update import명 수정
    import TripleIMG를 TripleImg로 변경

--- src/styles/image.ts
+++ src/styles/image.ts
@@ -1,7 +1,7 @@
-import TripleIMG from 'images/triple2x.png'
+import TripleImg from 'images/triple2x.png'
 import PlayStore from 'images/play-store2x.png'
 import AppStore from 'images/badge-apple4x.png'
 
-export const TRIPLE_LOGO_IMG = TripleIMG
+export const TRIPLE_LOGO_IMG = TripleImg
 export const PLAY_STORE_IMG = PlayStore
 export const APP_STORE_IMG = AppStore
