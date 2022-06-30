b0309413d01723f952bb80f8dd316a124a55cee3
KIMYUNMEE <jayoo8665@naver.com>
2022-07-01 04:48:31 +0900

    :truck: Update background-image명 변경
    background-image 파일명 변경 ico_*_*

--- src/styles/image.ts
+++ src/styles/image.ts
@@ -1,6 +1,6 @@
-import TripleImg from 'assets/images/triple2x.png'
-import PlayStore from 'assets/images/play-store2x.png'
-import AppStore from 'assets/images/badge-apple4x.png'
+import TripleImg from 'assets/images/ico_triple_logo.png'
+import PlayStore from 'assets/images/ico_circle_playstore.png'
+import AppStore from 'assets/images/ico_square_appstore.png'
 
 export const TRIPLE_LOGO_IMG = TripleImg
 export const PLAY_STORE_IMG = PlayStore
