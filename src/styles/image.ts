80e425210f7c89be3649066a0656bf370f07b3b2
KIMYUNMEE <jayoo8665@naver.com>
2022-06-30 18:09:52 +0900

    :truck: Update images 폴더 이동
    images 폴더 assets 폴더로 이동

--- src/styles/image.ts
+++ src/styles/image.ts
@@ -1,6 +1,6 @@
-import TripleImg from 'images/triple2x.png'
-import PlayStore from 'images/play-store2x.png'
-import AppStore from 'images/badge-apple4x.png'
+import TripleImg from 'assets/images/triple2x.png'
+import PlayStore from 'assets/images/play-store2x.png'
+import AppStore from 'assets/images/badge-apple4x.png'
 
 export const TRIPLE_LOGO_IMG = TripleImg
 export const PLAY_STORE_IMG = PlayStore
